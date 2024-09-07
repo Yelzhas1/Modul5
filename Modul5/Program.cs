38aa2ad490a557e961edfaed41f753765719d73d
Yelzhas1 <elzhas_1998@mail.ru>
2024-09-07 22:31:46 +0500

    Все до итогового задания

--- Modul5/Program.cs
+++ Modul5/Program.cs
@@ -86,15 +86,20 @@ class MainClass
         //BigDataOperation(in arr);
 
         //Console.WriteLine(arr[0]);
-        Console.WriteLine("Напишите что-то");
-        var str = Console.ReadLine();
 
-        Console.WriteLine("Укажите глубину эха");
-        var deep = int.Parse(Console.ReadLine());
+        //Console.WriteLine("Напишите что-то");
+        //var str = Console.ReadLine();
 
-        Echo(str, deep);
+        //Console.WriteLine("Укажите глубину эха");
+        //var deep = int.Parse(Console.ReadLine());
 
-        Console.ReadKey();
+        //Echo(str, deep);
+
+        //Console.ReadKey();
+
+        //Console.WriteLine(Factorial(20));
+        
+        Console.WriteLine(PowerUp(2,3));
     }
 
     static int[] GetArrayFromConsole()//ref int num
@@ -194,11 +199,43 @@ class MainClass
             modif = modif.Remove(0, 2);
         }
 
+        Console.BackgroundColor = (ConsoleColor)deep;
         Console.WriteLine("..." + modif);
 
         if (deep > 1)
         {
             Echo(modif, deep - 1);
+        }
+    }
+
+    static decimal Factorial(int x)
+    {
+        if (x == 0)
+        {
+            return 1;
+        }
+        else
+        {
+            return x * Factorial(x - 1);
+        }
+    }
+
+    private static int PowerUp(int N, byte pow)
+    {
+        if (pow == 0)
+        {
+            return 1;
+        }
+        else
+        {
+            if (pow == 1)
+            {
+                return N;
+            }
+            else
+            {
+                return N * PowerUp(N, --pow);
+            }
         }
     }
 }
